8ba89ed8565637906bbefbd70646494b5e595aff
Sean Ben <seanauditor0704@gmail.com>
2024-11-06 10:27:45 +0800

    feat: create game loop

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -3,15 +3,48 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
     public static final int WIDTH = 1260;
     public static final int HEIGHT = 720;
+    final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel(){
         //panel settings
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+    }
+
+    public void launchGame(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @override
+    public void run(){
+        //game loop
+        double drawInterval = 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime)/drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+    private void update(){}
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
     }
 
 
