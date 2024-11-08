d3eb0a2cbaff285826d16690980c9a8055609bf5
Sean Ben <seanauditor0704@gmail.com>
2024-11-09 04:14:04 +0800

    feat: add upPressed direction handling

--- src/Mino/Mino.java
+++ src/Mino/Mino.java
@@ -9,6 +9,7 @@ public class Mino {
     public Block block[] = new Block[4];
     public Block tempB[] = new Block[4];
     int autoDropCounter = 0;
+    public int direction = 1;
 
     public void create(Color color){
         block[0] = new Block(color);
@@ -22,7 +23,21 @@ public class Mino {
     }
 
     public void setXY(int x, int y){}
-    public void updateXY(int direction){}
+    public void getDirection1(){}
+    public void getDirection2(){}
+    public void getDirection3(){}
+    public void getDirection4(){}
+    public void updateXY(int direction){
+        this.direction = direction;
+        block[0].x = tempB[0].x;
+        block[0].y = tempB[0].y;
+        block[1].x = tempB[1].x;
+        block[1].y = tempB[1].y;
+        block[2].x = tempB[2].x;
+        block[2].y = tempB[2].y;
+        block[3].x = tempB[3].x;
+        block[3].y = tempB[3].y;
+    }
     public void  update(){
         if(KeyHandler.downPressed){
             block[0].y += Block.SIZE;
@@ -55,6 +70,17 @@ public class Mino {
             autoDropCounter = 0;
             KeyHandler.rightPressed = false;
         }
+
+        if(KeyHandler.upPressed){
+            switch (direction){
+                case 1: getDirection2();break;
+                case 2: getDirection3();break;
+                case 3: getDirection4();break;
+                case 4: getDirection1();break;
+            }
+            KeyHandler.upPressed = false;
+        }
+
         autoDropCounter++;// when counter increases, mino goes down on block height
 
         if(autoDropCounter == PlayManager.dropInterval){
